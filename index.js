330c5fb81035e2dbdfcb4649e7288b7d3212fbea
SharjeelJathol <66735435+SharjeelJathol@users.noreply.github.com>
2022-02-03 10:53:31 +0500

    Play sound with parameter

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 
+/*To play the respective sound*/
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
 /*Animation to buttons*/
 function animatePress(currentColour)
 {
